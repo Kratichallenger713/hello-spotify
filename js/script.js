f17d14d61bc3ec786171284b956454f81e08c135
Kratika Bathav <157878720+Kratichallenger713@users.noreply.github.com>
2025-06-15 01:03:56 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -19,42 +19,35 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
   currFolder = folder;
-  let a = await fetch(`/${folder}/`);
-  let response = await a.text();
-  let div = document.createElement("div");
-  div.innerHTML = response;
-  let as = div.getElementsByTagName("a");
-  songs = [];
-  for (let index = 0; index < as.length; index++) {
-    const element = as[index];
-    if (element.href.endsWith(".mp3")) {
-      songs.push(element.href.split(`/${folder}/`)[1]);
-    }
-  }
 
+  // ✅ Load songs list from songs.json
+  let a = await fetch(`/${folder}/songs.json`);
+  songs = await a.json();
+
+  // ✅ Populate UI
   let songUL = document
     .querySelector(".songlist")
     .getElementsByTagName("ul")[0];
   songUL.innerHTML = "";
   for (const song of songs) {
-    songUL.innerHTML =
-      songUL.innerHTML +
-      `<li><img class="invert" width="34" src="img/music.svg" alt="">
-                            <div class="info">
-                                <div> ${song.replaceAll("%20", " ")}</div>
-                                <div>Harry</div>
-                            </div>
-                            <div class="playnow">
-                                <span>Play Now</span>
-                                <img class="invert" src="img/play.svg" alt="">
-                            </div> </li>`;
+    songUL.innerHTML += `
+      <li>
+        <img class="invert" width="34" src="img/music.svg" alt="">
+        <div class="info">
+            <div>${song.replaceAll("%20", " ")}</div>
+            <div>Harry</div>
+        </div>
+        <div class="playnow">
+            <span>Play Now</span>
+            <img class="invert" src="img/play.svg" alt="">
+        </div>
+      </li>`;
   }
 
-  // Attach an event listener to each song
   Array.from(
     document.querySelector(".songlist").getElementsByTagName("li")
   ).forEach((e) => {
-    e.addEventListener("click", (element) => {
+    e.addEventListener("click", () => {
       playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim());
     });
   });
